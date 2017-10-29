aeaf1009901159714ce282581accca144511726b
LamornaJS <32608874+LamornaJS@users.noreply.github.com>
2017-10-29 18:44:19 +0000

    Added existing files for Tegen Short website

---
+++ work.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let likesDogs = true;
+
+let num1 = 3;
+let num2 = 11;
+let num3 = 43;
+let num4 = 69;
+
+
+
+let myArray = [num1, 'tesla', num2, 'whaterver', 'car', 'house'];
+
+for (var i = 0; i <= myArray.length; i += 2) {
+
+
+
+}
